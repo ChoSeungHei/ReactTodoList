6ce59891fb20baa591ca9514b63519ff04faaaa0
ChoSeungHei <kwa0403261@gmail.com>
2020-09-18 14:18:49 +0900

    Updates

---
+++ precache-manifest.f57fbe5285e3ac64e29949d282a472c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a228c7c2c29b5f1920c5428fc56ddd5f",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "46d8512f039007b98b3a",
+    "url": "/reacttodolist/static/css/main.e2a5ee18.chunk.css"
+  },
+  {
+    "revision": "6f3667654d25538bf9ba",
+    "url": "/reacttodolist/static/js/2.b408ff11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reacttodolist/static/js/2.b408ff11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d8512f039007b98b3a",
+    "url": "/reacttodolist/static/js/main.b67dc5d9.chunk.js"
+  },
+  {
+    "revision": "087e9a2440c0eb4c4576",
+    "url": "/reacttodolist/static/js/runtime-main.87a7b0fe.js"
+  }
+]);
